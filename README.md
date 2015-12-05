20404c1c9f4e59afcfaf91f9aea1e50ff6545f70
vtst <github.self@vtst.net>
2015-12-05 09:25:00 -0800

    Initial version

--- README.md
+++ README.md
@@ -1,2 +1,68 @@
-# python-delegating-http-server
-An HTTP server that serves local files and act as a proxy for another HTTP server
+# python-dhttpd
+An HTTP server that serves local files and act as a proxy for another
+HTTP server.
+
+Overview
+========
+
+python-dhttpd is a simple HTTP server, implemented on top of
+[`BaseHTTPServer.HTTPServer`](https://docs.python.org/2/library/basehttpserver.html)
+that serves files from a local directory (like
+[`SimpleHTTPServer`](https://docs.python.org/2/library/simplehttpserver.html))
+or by forwarding HTTP request to a delegate HTTP server.
+
+For every GET or HEAD request, python-dhttpd first looks for a local
+file to serve.  If a file is found, it is returned.  If not, the
+request is forwarded to the delegate HTTP server.
+
+Other types of HTTP requests are systematically forwarded.
+
+Usage
+=====
+
+```
+usage: dhttpd.py [-h] [--dserver HOST:PORT] [-p PORT] [-l DIR]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --dserver HOST:PORT   Delegate HTTP server
+  -p PORT, --port PORT  Port to listen
+  -l DIR, --localdir DIR
+                        Local directory to serve
+```
+
+Using with App Engine devserver and Maven
+=========================================
+
+The Maven App Engine archetypes include a development server that
+allows to run the App Engine application locally.  This development
+server is automatically updated when Java classe files are modified.
+However, it is *not* updated when static files (like HTML or
+JavaScript files) are updated.
+
+You can use python-dhttpd to work around this problem as follows:
+
+```shell
+cd <root-path-of-your-maven-project>
+# Start the devserver
+mvn appengine:devserver
+# Start dhttpd
+dhttpd.py --localdir=src/main/webapp
+```
+
+Then, access the App Engine application running on the devserver on
+the port 8081 (instead of the default port 8080) by entering the
+following address in your web browser:
+
+```
+http://localhost:8081/
+```
+
+
+License
+=======
+
+This software is distributed under the terms of the [Apache Software
+License, version 2.0](http://www.apache.org/licenses/LICENSE-2.0).
+
+© Vincent Simonet, 2015.
---
+++ dhttpd.py
@@ -0,0 +1,185 @@
+#!/usr/bin/python
+# (c) Vincent Simonet, 2015.
+
+import argparse
+import httplib
+import mimetypes
+import os
+import posixpath
+import shutil
+import threading
+import urllib
+import BaseHTTPServer
+import SimpleHTTPServer
+from SocketServer import ThreadingMixIn
+
+
+class DelegatingHTTPServer(BaseHTTPServer.HTTPServer):
+
+  def __init__(self, server_address,
+               RequestHandlerClass,
+               dserver_address,
+               localdir):
+    BaseHTTPServer.HTTPServer.__init__(self,
+                                       server_address, RequestHandlerClass)
+    self.dserver_host = dserver_address[0]
+    self.dserver_port = dserver_address[1]
+    self.localdir = os.path.abspath(localdir)
+
+
+class ThreadedDelegatingHTTPServer(ThreadingMixIn, DelegatingHTTPServer):
+    """Handle requests in a separate thread."""
+
+
+class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+
+  _FILTERED_HEADERS = set(['connection', 'host'])
+
+  def send_local(self):
+    path = self.translate_path(self.path)
+    f = None
+    if os.path.isdir(path):
+      for index in "index.html", "index.htm":
+        index = os.path.join(path, index)
+        if os.path.exists(index):
+          path = index
+          break
+        else:
+          return False
+    ctype = self.guess_type(path)
+    if ctype.startswith('text/'):
+      mode = 'r'
+    else:
+      mode = 'rb'
+    try:
+      f = open(path, mode)
+    except IOError:
+      return False
+    self.send_response(200)
+    self.send_header("Content-type", ctype)
+    self.end_headers()
+    self.copyfile(f, self.wfile)
+    f.close()
+    return True
+
+  def translate_path(self, path):
+    path = posixpath.normpath(urllib.unquote(path))
+    words = path.split('/')
+    words = filter(None, words)
+    path = self.server.localdir
+    for word in words:
+      drive, word = os.path.splitdrive(word)
+      head, word = os.path.split(word)
+      if word in (os.curdir, os.pardir): continue
+      path = os.path.join(path, word)
+    return path
+
+  def copyfile(self, source, outputfile):
+    shutil.copyfileobj(source, outputfile)
+
+  def guess_type(self, path):
+    base, ext = posixpath.splitext(path)
+    if self.extensions_map.has_key(ext):
+      return self.extensions_map[ext]
+    ext = ext.lower()
+    if self.extensions_map.has_key(ext):
+      return self.extensions_map[ext]
+    else:
+      return self.extensions_map['']
+
+  extensions_map = mimetypes.types_map.copy()
+  extensions_map.update({
+    '': 'application/octet-stream', # Default
+    '.py': 'text/plain',
+    '.c': 'text/plain',
+    '.h': 'text/plain',
+  })
+
+  def do_LOCAL_or_PROXY(self):
+    if not self.send_local():
+      self.do_PROXY()
+
+  def do_PROXY(self):
+    conn = httplib.HTTPConnection(self.server.dserver_host,
+                                  self.server.dserver_port)
+    content_len = int(self.headers.get('content-length', 0))
+    body = self.rfile.read(content_len)
+    headers = dict((key, self.headers.get(key)) for key in self.headers
+                   if not key in self._FILTERED_HEADERS)
+    conn.request(self.command, self.path, body, headers)
+    resp = conn.getresponse()
+    self.send_response(resp.status, resp.reason)
+    for name, value in resp.getheaders():
+      self.send_header(name, value)
+    self.end_headers()
+    self.wfile.write(resp.read())
+    conn.close()
+
+  do_GET     = do_LOCAL_or_PROXY
+  do_POST    = do_PROXY
+  do_HEAD    = do_LOCAL_or_PROXY
+  do_PUT     = do_PROXY
+  do_DELETE  = do_PROXY
+  do_OPTIONS = do_PROXY
+
+
+_DESCRIPTION = """
+An HTTP server that serves files from a local directory and a delegate HTTP
+server.  For every request, the server first look for a local file to search.
+If a file is found, it is returned directly.  If no local file is found, the
+request is forwarded to the delegate HTTP server.
+"""
+
+
+def _parse_host_port(s):
+  parts = s.split(':')
+  if len(parts) == 1:
+    return parts[0], 80
+  else:
+    return parts[0], int(parts[1])
+
+
+def main():
+  parser = argparse.ArgumentParser(description=_DESCRIPTION)
+  parser.add_argument('--dserver',
+                      action='store',
+                      type=str,
+                      dest='dserver',
+                      help='Delegate HTTP server',
+                      metavar='HOST:PORT',
+                      default='localhost:8080')
+  parser.add_argument('-p', '--port',
+                      action='store',
+                      type=int,
+                      dest='port',
+                      help='Port to listen',
+                      metavar='PORT',
+                      default='8081')
+  parser.add_argument('-l', '--localdir',
+                      action='store',
+                      type=str,
+                      dest='localdir',
+                      help='Local directory to serve',
+                      metavar='DIR',
+                      default='.')
+  args = parser.parse_args()
+
+  server_address = ('', args.port)
+  dserver_address = _parse_host_port(args.dserver)
+  httpd = ThreadedDelegatingHTTPServer(server_address,
+                                       RequestHandler,
+                                       dserver_address,
+                                       args.localdir)
+  httpd.daemon_threads = True
+  print 'Listening port ' + str(args.port)
+  try:
+    httpd.serve_forever()
+  except KeyboardInterrupt:
+    httpd.shutdown()
+    print '\nThat\'s all folks!'
+
+if __name__ == '__main__':
+  main()
+
+
+
